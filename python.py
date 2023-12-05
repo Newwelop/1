5749a088c553a3931d892107c348f1660c3c3155
python <maksdavid2009@gmail.com>
2023-12-05 19:16:27 +0100

    5.32.23_2

--- python.py
+++ python.py
@@ -1,3 +1,11 @@
-print("Hello")
-print("world")
-print("pon")
+def чи_парне_число(number):
+    if (number % 2 == 0):
+        return "парне"
+    else:
+        return "непарне"
+
+number = input("Введіть число: ")
+number = int(number)
+
+результат = чи_парне_число(number)
+print(результат)
